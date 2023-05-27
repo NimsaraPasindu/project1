274e1ca1fa20fca960bb0b9cf01aef7a562284fe
nmeshamedagoda0@gmail.com <nimeshamedagoda0@gmail.com>
2023-05-27 09:31:58 +0530

    first commit

---
+++ s1.js
@@ -0,0 +1,21 @@
+console.log(typeof(9));
+ //number
+
+console.log(12+26);
+//38
+
+let num1=10;
+
+let num2=5;
+
+console.log(num1+num2);
+//15
+
+console.log(1+3*4-2)
+//11
+
+console.log(1+3)*(4-2)
+//4
+
+console.log((1+3)*(4-2));
+//8
